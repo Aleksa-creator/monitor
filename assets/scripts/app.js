5f0eebe12153faa3ba624b3cec27139fbc9cf7cb
Aleksa-creator <alexleps346@gmail.com>
2024-07-14 20:39:47 -0700

    I have done some block and it is pretty near to finish

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+menu.onClick = function MyFunction(){
+	var x = document.getElementById('myTopnav');
+
+	if(x.className === "topnav"){
+		x.className += " responsive";
+	} else{
+		x.className = "topnav";
+	}
+}
